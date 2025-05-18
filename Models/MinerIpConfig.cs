72e80f811178fe200374d81d55bcb34353a81317
John Doe <johndoe@email.com>
2025-05-17 21:59:26 -0500

    add miner

--- Models/MinerIpConfig.cs
+++ Models/MinerIpConfig.cs
@@ -1,17 +1,112 @@
 ﻿namespace SetMinerStaticIpCK.Models;
 
-public class MinerIpConfig(
-    int lineNo,
-    string currentIp,
-    string maCAddress,
-    string targetIp,
-    string setStatus,
-    string minerMode)
+using System.ComponentModel;
+
+public class MinerIpConfig : INotifyPropertyChanged
 {
-    public int LineNo { get; set; } = lineNo;
-    public string CurrentIp { get; set; } = currentIp;
-    public string MaCAddress { get; set; } = maCAddress;
-    public string TargetIp { get; set; } = targetIp;
-    public string SetStatus { get; set; } = setStatus;
-    public string MinerMode { get; set; } = minerMode;
+    private int _lineNo;
+
+    public int LineNo
+    {
+        get => _lineNo;
+        set
+        {
+            if (_lineNo != value)
+            {
+                _lineNo = value;
+                OnPropertyChanged(nameof(LineNo));
+            }
+        }
+    }
+
+    private string _currentIp;
+
+    public string CurrentIp
+    {
+        get => _currentIp;
+        set
+        {
+            if (_currentIp != value)
+            {
+                _currentIp = value;
+                OnPropertyChanged(nameof(CurrentIp));
+            }
+        }
+    }
+
+    private string _maCAddress;
+
+    public string MaCAddress
+    {
+        get => _maCAddress;
+        set
+        {
+            if (_maCAddress != value)
+            {
+                _maCAddress = value;
+                OnPropertyChanged(nameof(MaCAddress));
+            }
+        }
+    }
+
+    private string _targetIp;
+
+    public string TargetIp
+    {
+        get => _targetIp;
+        set
+        {
+            if (_targetIp != value)
+            {
+                _targetIp = value;
+                OnPropertyChanged(nameof(TargetIp));
+            }
+        }
+    }
+
+    private string _setStatus;
+
+    public string SetStatus
+    {
+        get => _setStatus;
+        set
+        {
+            if (_setStatus != value)
+            {
+                _setStatus = value;
+                OnPropertyChanged(nameof(SetStatus));
+            }
+        }
+    }
+
+    private string _minerMode;
+
+    public string MinerMode
+    {
+        get => _minerMode;
+        set
+        {
+            if (_minerMode != value)
+            {
+                _minerMode = value;
+                OnPropertyChanged(nameof(MinerMode));
+            }
+        }
+    }
+
+    public MinerIpConfig(int lineNo, string currentIp, string maCAddress, string targetIp, string setStatus,
+        string minerMode)
+    {
+        _lineNo = lineNo;
+        _currentIp = currentIp;
+        _maCAddress = maCAddress;
+        _targetIp = targetIp;
+        _setStatus = setStatus;
+        _minerMode = minerMode;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,19 +2,25 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System;
+using System.Reactive;
+using System.Threading;
+using Avalonia.Threading;
+
+namespace SetMinerStaticIpCK.ViewModels;
+
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using ReactiveUI;
-using System.Reactive;
-
-namespace SetMinerStaticIpCK.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
     public ObservableCollection<Person> People { get; }
-    public ObservableCollection<MinerIpConfig> MinerIpList { get; }
+    public ObservableCollection<MinerIpConfig> MinerIpList { get; set; }
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+    public ICommand StartUdpCommand { get; }
 
     public MainWindowViewModel()
     {
@@ -30,39 +36,80 @@ public class MainWindowViewModel : ViewModelBase
             new MinerIpConfig(1, @"10.11.10.1", "mac", @"10.11.1.1", "wait", "S19")
         };
         MinerIpList = new ObservableCollection<MinerIpConfig>(minerIpList);
+        SaveCommand = ReactiveCommand.Create(SaveData);
+        StartUdpCommand = ReactiveCommand.Create(StartUdpListener);
     }
 
-    private string _log = "等待监听...\n";
+    private string _description = string.Empty;
 
-    public string Log
+    public string Description
     {
-        get => _log;
-        set => this.RaiseAndSetIfChanged(ref _log, value);
+        get => _description;
+        set => this.RaiseAndSetIfChanged(ref _description, value);
     }
 
-    private void AppendLog(string text)
+    public void ButtonAction()
     {
-        Log += $"{DateTime.Now:T} - {text}\n";
+        MinerIpList[0].SetStatus = "来自直接调用正在更新中";
+
+        DoWorkInBackground();
     }
 
-    private void StartListening()
+    public async void DoWorkInBackground()
     {
-        Task.Run(async () =>
+        await Task.Run(() =>
+        {
+            // 假设是一个长时间操作
+            Thread.Sleep(2000);
+
+            // 切换到UI线程来更新绑定属性
+            Dispatcher.UIThread.Post(() =>
+            {
+                Description = "后台任务完成，来自UI线程的问候";
+                MinerIpList[0].SetStatus = "后台任务完成，正在更新中";
+                MinerIpList.Add(new MinerIpConfig(
+                    2,
+                    "10.11.10.2",
+                    "aa:bb:cc:dd:ee:ff",
+                    "10.11.1.2",
+                    "running",
+                    "S21"
+                ));
+            });
+        });
+    }
+
+    private void SaveData()
+    {
+        foreach (var miner in MinerIpList)
+        {
+            Console.WriteLine($"CurrentIp: {miner.CurrentIp}, LineNo: {miner.LineNo}, Status: {miner.SetStatus}");
+        }
+
+        // 你也可以在这里写入文件，例如 CSV、JSON 等。
+    }
+
+    private void StartUdpListener()
+    {
+        Task.Run(() =>
         {
             using var udpClient = new UdpClient(14235);
-            AppendLog("UDP 监听已启动，端口: 14235");
+            var remoteEP = new IPEndPoint(IPAddress.Any, 0);
 
             while (true)
             {
                 try
                 {
-                    var result = await udpClient.ReceiveAsync();
-                    string message = Encoding.UTF8.GetString(result.Buffer);
-                    AppendLog($"收到来自 {result.RemoteEndPoint}: {message}");
+                    var data = udpClient.Receive(ref remoteEP);
+                    var message = Encoding.UTF8.GetString(data);
+
+                    Console.WriteLine($"收到UDP数据：{message} 来自：{remoteEP}");
+                    // 可在此调用 Dispatcher/UI 更新界面内容
                 }
                 catch (Exception ex)
                 {
-                    AppendLog($"错误: {ex.Message}");
+                    Console.WriteLine($"UDP监听异常: {ex.Message}");
+                    break;
                 }
             }
         });
